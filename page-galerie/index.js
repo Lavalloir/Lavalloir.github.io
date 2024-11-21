25ea8497d0b5d4d630304d59a72f8a4b8b357b74
Clément Lavalloir <clement.lavalloir@ccicampus.fr>
2024-11-21 15:49:40 +0100

    commit

--- page-galerie/index.js
+++ page-galerie/index.js
@@ -5,3 +5,11 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+$(function () {
+  $(".gallery img").click(function () {
+    // console.log($(this)[0].src);
+    $("#modal-div img")[0].src = $(this)[0].src;
+    $("#modal-div").modal();
+  });
+});
